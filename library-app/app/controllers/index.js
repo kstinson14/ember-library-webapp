d5b4b54fdfd065dbb4b674686998ac776f9409bb
Katrina Stinson <katrina.m.stinson@gmail.com>
2017-07-07 17:08:10 -0400

    Added control on home page to validate user's email address and give them a response message.

---
+++ library-app/app/controllers/index.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+  emailAddress: '',
+  isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: Ember.computed.not('isValid'),
+  actions: {
+    saveInvitation() {
+      alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
+      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+      this.set('emailAddress', '');
+    }
+  }
+});
